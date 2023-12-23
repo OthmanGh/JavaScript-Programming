bd6e58fd0d33ff4cb6c26d3515f8444e5fe2b845
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-24 00:46:54 +0300

    basic dom implementation

--- introDOM/Guess-My-Number/script.js
+++ introDOM/Guess-My-Number/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// console.log(message.textContent);
+
+// * DOM (Document Object Model) : structured representation of html documents allows javascript to access html elements and styles to manipulate them
+
+// ? DOM stored in a tree structure form each html element represented as object
+
+// * document is a speical obejct that we've access to in js & it serve as entry point into the DOM
+
+// * DOM is not a part of javascript, it is a part of WEB APIs
+
+/*
+Basic DOM manipulation : 
+console.dir(document);
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.number').textContent = 23;
+document.querySelector('.message').textContent = 'Correct answer!!';
+
+document.querySelector('.guess').value = 10;
+console.log(document.querySelector('.guess').value);
+*/

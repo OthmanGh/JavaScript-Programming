921cf64dd2d6ac3a06e99a8ce17a4cbe54440e00
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-25 16:21:22 +0300

    refactor code

--- introDOM/Modal/script.js
+++ introDOM/Modal/script.js
@@ -6,24 +6,21 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const closeBtn = document.querySelector('.close-modal');
 
+// Functions:
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 // Event Listeners :
 showBtns.forEach(btn => {
-  btn.addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btn.addEventListener('click', showModal);
 });
 
-closeBtn.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
-
-overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-});
-
-// document.addEventListener('keydown', function (e) {
-//   console.log(e.key);
-// });
+closeBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);

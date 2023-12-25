f2d0aa647a1267216e01e9da63340add3f416d05
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-12-25 16:17:40 +0300

    implement show modal basic functionality

--- introDOM/Modal/script.js
+++ introDOM/Modal/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+// Selecting Elements :
+const showBtns = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close-modal');
+
+// Event Listeners :
+showBtns.forEach(btn => {
+  btn.addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+});
+
+closeBtn.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+// document.addEventListener('keydown', function (e) {
+//   console.log(e.key);
+// });
